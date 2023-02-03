9b5f9719ddd7cab200bb7793e5132bb3053c54d7
Patrick <67484198+patrickjm7@users.noreply.github.com>
2023-02-02 22:37:30 -0700

    Initial commit

---
+++ JS/index.js
@@ -0,0 +1,28 @@
+ // Intro 
+$("#patrickA").hover(function() {
+    $("#intro h1").css("color", "#525252");
+    $("#intro a").css("color", "#525252");
+    $(this).css("color", "#e26145");
+    }, function() {
+    $("#intro h1").css("color", "#f5f5f5");
+    $("#intro a").css("color", "#e26145");
+});
+
+$("#workA").hover(function() {
+    $("#intro h1").css("color", "#525252");
+    $("#intro a").css("color", "#525252");
+    $(this).css("color", "#e26145");
+    }, function() {
+    $("#intro h1").css("color", "#f5f5f5");
+    $("#intro a").css("color", "#e26145");
+});
+
+$("#contactA").hover(function() {
+    $("#intro h1").css("color", "#525252");
+    $("#intro a").css("color", "#525252");
+    $(this).css("color", "#e26145");
+    }, function() {
+    $("#intro h1").css("color", "#f5f5f5");
+    $("#intro a").css("color", "#e26145");
+});
+// End intro
